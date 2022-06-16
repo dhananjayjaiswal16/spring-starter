179d7a4fc9891e1910c3b972756e5b48fdafa922
dhananjayjaiswal16 <dhananjayjaiswal16.10@gmail.com>
2022-06-16 10:49:56 +0530

    scope: prototype with Proxy

--- src/main/java/com/exmaple/SpringFramework/SpringFrameworkScopeApplication.java
+++ src/main/java/com/exmaple/SpringFramework/SpringFrameworkScopeApplication.java
@@ -16,16 +16,12 @@ public class SpringFrameworkScopeApplication {
 				SpringApplication.run(SpringFrameworkBasicApplication.class, args);
 		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
 		PersonDAO personDAO1 = applicationContext.getBean(PersonDAO.class);
-		PersonDAO personDAO3 = applicationContext.getBean(PersonDAO.class);
 
 		LOGGER.info("{}", personDAO);
 		LOGGER.info("{}", personDAO.getJdbcConnection());
 
 		LOGGER.info("{}", personDAO1);
 		LOGGER.info("{}", personDAO1.getJdbcConnection());
-
-		LOGGER.info("{}", personDAO3);
-		LOGGER.info("{}", personDAO3.getJdbcConnection());
 
 	}
 }
