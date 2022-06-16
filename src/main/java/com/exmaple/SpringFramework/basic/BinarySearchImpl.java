c37d3416274202925d97a5f9420e9ca00e9e732e
dhananjayjaiswal16 <dhananjayjaiswal16.10@gmail.com>
2022-06-16 12:18:06 +0530

    PostConstruct and PreDestroy

--- src/main/java/com/exmaple/SpringFramework/basic/BinarySearchImpl.java
+++ src/main/java/com/exmaple/SpringFramework/basic/BinarySearchImpl.java
@@ -1,10 +1,16 @@
 package com.exmaple.SpringFramework.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class BinarySearchImpl {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
@@ -15,6 +21,18 @@ public class BinarySearchImpl {
 		System.out.println(sortAlgorithm);
 		// Search the array
 		return 3;
+	}
+
+	//As soon as BEan is created the postConstruct method is called
+	@PostConstruct
+	public void postConstruct(){
+		logger.info("PostConstruct called");
+	}
+
+	//preDestroy is called just before bean is removed from the context
+	@PreDestroy
+	public void preDestroy(){
+		logger.info("PreDestroy called");
 	}
 
 }
