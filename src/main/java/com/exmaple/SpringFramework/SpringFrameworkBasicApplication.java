240d42e612e1387309c4c1b858448f15c4b87809
dhananjayjaiswal16 <dhananjayjaiswal16.10@gmail.com>
2022-06-16 10:40:42 +0530

    scope: prototype

--- src/main/java/com/exmaple/SpringFramework/SpringFrameworkBasicApplication.java
+++ src/main/java/com/exmaple/SpringFramework/SpringFrameworkBasicApplication.java
@@ -1,31 +1,19 @@
 package com.exmaple.SpringFramework;
 
-import com.exmaple.SpringFramework.scope.PersonDAO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.exmaple.SpringFramework.basic.BinarySearchImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringFrameworkBasicApplication {
-
-	private static Logger LOGGER = LoggerFactory.getLogger(SpringFrameworkScopeApplication.class);
-	public static void main(String[] args) {
-		ApplicationContext applicationContext = 
-				SpringApplication.run(SpringFrameworkScopeApplication.class, args);
-		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
-		PersonDAO personDAO1 = applicationContext.getBean(PersonDAO.class);
-		PersonDAO personDAO3 = applicationContext.getBean(PersonDAO.class);
-
-		LOGGER.info("{}", personDAO);
-		LOGGER.info("{}", personDAO.getJdbcConnection());
-
-		LOGGER.info("{}", personDAO1);
-		LOGGER.info("{}", personDAO1.getJdbcConnection());
-
-		LOGGER.info("{}", personDAO3);
-		LOGGER.info("{}", personDAO3.getJdbcConnection());
-
-	}
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                SpringApplication.run(SpringFrameworkScopeApplication.class, args);
+        BinarySearchImpl binarySearch =
+                applicationContext.getBean(BinarySearchImpl.class);
+        int result =
+                binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
+        System.out.println(result);
+    }
 }
--- src/main/java/com/exmaple/SpringFramework/SpringFrameworkScopeApplication.java
+++ src/main/java/com/exmaple/SpringFramework/SpringFrameworkScopeApplication.java
@@ -1,19 +1,31 @@
 package com.exmaple.SpringFramework;
 
-import com.exmaple.SpringFramework.basic.BinarySearchImpl;
+import com.exmaple.SpringFramework.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringFrameworkScopeApplication {
-    public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                SpringApplication.run(SpringFrameworkBasicApplication.class, args);
-        BinarySearchImpl binarySearch =
-                applicationContext.getBean(BinarySearchImpl.class);
-        int result =
-                binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-        System.out.println(result);
-    }
+
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringFrameworkBasicApplication.class);
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = 
+				SpringApplication.run(SpringFrameworkBasicApplication.class, args);
+		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
+		PersonDAO personDAO1 = applicationContext.getBean(PersonDAO.class);
+		PersonDAO personDAO3 = applicationContext.getBean(PersonDAO.class);
+
+		LOGGER.info("{}", personDAO);
+		LOGGER.info("{}", personDAO.getJdbcConnection());
+
+		LOGGER.info("{}", personDAO1);
+		LOGGER.info("{}", personDAO1.getJdbcConnection());
+
+		LOGGER.info("{}", personDAO3);
+		LOGGER.info("{}", personDAO3.getJdbcConnection());
+
+	}
 }
